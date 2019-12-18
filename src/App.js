643b88701aaf643405c0c16fd0b92b99ebcf04a1
OmarYuusf <57051225+OmarYuusf@users.noreply.github.com>
2019-12-17 18:38:18 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
+import "./App.css"
 
 class App extends Component{
-
   render(){
     return (
-      <> 
-        Hello React 
-      </>
+      <div className="App">
+        <div className="form">
+            <input type="text" placeholder="name" />
+            <input type="number" placeholder="Age" />
+            <button>Submit</button>
+        </div>
+        <div className="container-persons">
+            <div className="person">
+                <h6>Omar Yuusf</h6>
+                <p>22</p>
+            </div>
+            <div className="person">
+              <h6>Omar Yuusf</h6>
+              <p>22</p>
+            </div>
+            <div className="person">
+              <h6>Omar Yuusf</h6>
+              <p>22</p>
+            </div>
+            <div className="person">
+              <h6>Omar Yuusf</h6>
+              <p>22</p>
+            </div><div className="person">
+              <h6>Omar Yuusf</h6>
+              <p>22</p>
+            </div>
+        </div>
+      </div>
     )
   }
 }
