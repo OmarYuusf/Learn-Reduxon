92fafc00a9c26c776ec9fcf75aae8a0296823946
OmarYuusf <57051225+OmarYuusf@users.noreply.github.com>
2019-12-19 00:40:17 -0800

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
-import React, {Component} from 'react';
-import "./App.css"
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom"
 
-class App extends Component{
-  render(){
+import "./App.css";
+
+class App extends Component {
+  render() {
     return (
-      <div className="App">
-        <div className="form">
-            <input type="text" placeholder="name" />
-            <input type="number" placeholder="Age" />
-            <button>Submit</button>
+      <BrowserRouter>
+        <div className="quality">
+          hello
         </div>
-        <div className="container-persons">
-            <div className="person">
-                <h6>Omar Yuusf</h6>
-                <p>22</p>
-            </div>
-            <div className="person">
-              <h6>Omar Yuusf</h6>
-              <p>22</p>
-            </div>
-            <div className="person">
-              <h6>Omar Yuusf</h6>
-              <p>22</p>
-            </div>
-            <div className="person">
-              <h6>Omar Yuusf</h6>
-              <p>22</p>
-            </div><div className="person">
-              <h6>Omar Yuusf</h6>
-              <p>22</p>
-            </div>
-        </div>
-      </div>
-    )
+      </BrowserRouter>
+    );
   }
 }
 
